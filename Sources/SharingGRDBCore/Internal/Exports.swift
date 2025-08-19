4a5b11ec147d6e5aa24a1d62f04ccbb068f148d3
Stephen Celis <stephen@stephencelis.com>
2025-08-19 12:56:16 -0700

    wip

--- Sources/SharingGRDBCore/Internal/Exports.swift
+++ Sources/SharingGRDBCore/Internal/Exports.swift
@@ -3,8 +3,8 @@
 @_exported import StructuredQueriesGRDBCore
 
 @_exported import class GRDB.Database
-@_exported import class GRDB.DatabaseQueue
 @_exported import class GRDB.DatabasePool
+@_exported import class GRDB.DatabaseQueue
 @_exported import protocol GRDB.DatabaseReader
 @_exported import protocol GRDB.DatabaseWriter
 @_exported import protocol GRDB.ValueObservationScheduler
